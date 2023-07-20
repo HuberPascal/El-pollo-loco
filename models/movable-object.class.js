2eef55376dca1595f49bc9677b07b1403f15217f
Pascal Huber <pascalhuber@Pascals-MacBook-Pro.local>
2023-07-20 20:49:00 +0200

    status-bar, throwable-objects

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,16 +1,10 @@
-class MovableObject {
-    x = 120;
-    y = 280;
-    img;
-    height = 150;
-    width = 100;
-    imageCache = {};
-    currentImage = 0;
+class MovableObject extends DrawableObject {
     speed = 0.15;
     otherDirection = false;
     speedY = 0;
     acceleration = 2.5;
     energy = 100;
+    lastHit = 0;
 
 
     applyGravity() {
@@ -23,28 +17,10 @@ class MovableObject {
     }
 
     isAboveGround() {
+        if(this instanceof TrowableObject) { // TrowableObject should always fall
+            return true;
+        } else {
         return this.y < 120;
-    }
-
-
-    // loadImage('img/test.png')
-    loadImage(path) {
-        this.img = new Image(); // this.img = document.getElementById('image') <img id="image" scr>
-        this.img.src = path;
-    }
-
-    draw(ctx) {
-        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-    }
-
-    drawFrame(ctx) {
-
-        if(this instanceof Character || this instanceof Chicken) {
-            ctx.beginPath();
-            ctx.lineWidth = '5';
-            ctx.strokeStyle = 'blue';
-            ctx.rect(this.x, this.y, this.width, this.height);
-            ctx.stroke();
         }
     }
 
@@ -56,34 +32,44 @@ class MovableObject {
             this.y < mo.y + mo.height
     }
 
-    /**
-     * 
-     * @param {Array} arr - ['img/image1.png'. 'img/image2.png', ...]
-     */
-    loadImages(arr) {
-        arr.forEach((path) => {
-            let img = new Image();
-            img.src = path;
-            this.imageCache[path] = img;
-    });
-}
+    hit() {
+        this.energy -= 5;
+        if(this.energy < 0) {
+            this.energy = 0;
+        } else {
+            this.lastHit = new Date().getTime();
+        }
+    }
+
+    isHurt() {
+        let timepassed = new Date().getTime() - this.lastHit; // Differenz in ms
+        timepassed = timepassed / 1000; // Differenz in s
+        return timepassed < 1;
+    }
+
+
+    isDead(){
+        return this.energy == 0;
+    }
+
 
     playAnimation(images) {
-            let i = this.currentImage % this.IMAGES_WALKING.length; // let i = 7 % 6; => 1, Rest 1
+            let i = this.currentImage % images.length; // let i = 7 % 6; => 1, Rest 1
             let path = images[i];
             this.img = this.imageCache[path];
             this.currentImage++; // Inkrementierung von currentImage hinzugefügt
     }
 
 
-
     moveRight() {
         this.x += this.speed;
     }
 
+
     moveLeft() {
             this.x -= this.speed;
     }
+    
 
     jump() {
         this.speedY = 30;
--- models/world.class.js
+++ models/world.class.js
@@ -5,6 +5,8 @@ class World {
     ctx;
     keyboard;
     camera_x = 0;
+    statusBar = new StatusBar();
+    throwableObjects = [];
 
     constructor(canvas, keyboard) {
         this.ctx = canvas.getContext('2d');
@@ -12,22 +14,36 @@ class World {
         this.keyboard = keyboard;
         this.draw();
         this.setWorld();
-        this.checkCollisions();
+        this.run();
     }
 
     setWorld() {
         this.character.world = this;
     }
 
-    checkCollisions() {
+    run() {
         setInterval(() => {
-            this.level.enemies.forEach((enemy) => {
-                if(this.character.isColliding(enemy)) {
-                    this.character.energy -= 5;
-                    console.log('Collison with Character', this.character.energy);
-                }
-        });
+
+            this.checkCollisions();
+            this.checkThrowObjects();
         }, 200);
+    }
+
+
+    checkThrowObjects() {
+        if(this.keyboard.D) {
+            let bottle = new TrowableObject(this.character.x + 100, this.character.y + 100);
+            this.throwableObjects.push(bottle);
+        }
+    }
+
+    checkCollisions() {
+        this.level.enemies.forEach((enemy) => {
+            if(this.character.isColliding(enemy)) {
+                this.character.hit();
+                this.statusBar.setPercentage(this.character.energy);
+            }
+        });
     }
 
 
@@ -35,12 +51,17 @@ class World {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
         this.ctx.translate(this.camera_x, 0);
-
         this.addObjectsToMap(this.level.BackgroundObjects);
+
+        this.ctx.translate(-this.camera_x, 0);
+        // ------ Space for fixed objects ------
+        this.addToMap(this.statusBar);
+        this.ctx.translate(this.camera_x, 0);
         
         this.addToMap(this.character); 
         this.addObjectsToMap(this.level.clouds);
-        this.addObjectsToMap(this.level.enemies);
+        this.addObjectsToMap(this.level.enemies); 
+        this.addObjectsToMap(this.throwableObjects);
 
         this.ctx.translate(-this.camera_x, 0);
 
